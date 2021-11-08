7d36d860a7edd873462f3de936ff234881408add
JoshuaJWatt <48531152+JoshuaJWatt@users.noreply.github.com>
2021-11-08 12:02:04 +0000

    added comments and contacts. all html files are now php

---
+++ about-me.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <style>
+        div.gallery {
+          margin: 5px;
+          border: 1px solid #ccc;
+          float: left;
+          width: 180px;
+        }
+        
+        div.gallery:hover {
+          border: 1px solid #777;
+        }
+        
+        div.gallery img {
+          width: 100%;
+          height: auto;
+        }
+        
+        div.desc {
+          padding: 15px;
+          text-align: center;
+        }
+        </style>
+</head>
+<body>
+    <h1> The Pineapple Family</h1>
+
+    <div>
+        <img src="img/PHeart1.jpg" width="100%" height="50%" alt=" ">
+    </div>
+
+    <div>
+        <button type="button"><a href= index.php> Homepage </a></button>
+        <button type="button"><a href= gallery.php target="_blank"> Gallery </a></button>
+        <button type="button"><a href= contact.php target="_blank">Contact </a></button>
+    </div>
+
+    <div class="about1">
+        <p>
+            My binomial name is Ananas Comosus. I am a tropical plant with an edible fruit and is the most economically significant plant in the family Bromeliaceae. 
+        </p><br/>
+        <hr/>
+    </div>
+
+    <div class="about1">    
+        <p>
+            We pineapples take about 18-20 months to become ready to harvest. Pineapples are native to South America before Christopher Columbus discovered us in 1493.
+        </p>
+      
+    </div>
+        <hr/>
+
+        <h1><i>Pineapple Types and Origins</i></h1>
+
+        <div>
+        <p class="about1">
+            You will probably love pronouncing my Botanical Names!
+        </p>  
+    </div>
+
+    <div>
+        <table border="1">
+            <tr>
+                <th colspan="5">Botanical Varieties </th>
+            </tr>
+            <tr>
+                <td>Ananassoides</td>
+                <td>Bracteatus</td>
+                <td>Comosus</td>
+                <td>Erectifolius</td>
+                <td>Parguazensis</td>
+            </tr>
+        </table>
+    </div>
+    
+    <div>
+        <table border="1">
+            <tr>
+                <th colspan="4">Origins</th>
+            </tr>
+            <br/>
+            <tr>
+                <td>Hawaii</td>
+                <td>Costa Rica</td>
+                <td>Brazil</td>
+                <td>Philippines</td>
+            </tr> 
+
+            <hr/>
+        </table> 
+    </div>    
+
+    <div class="gallery">
+            <img src="img/Brazil1.jpg" width="1000" height="600" alt="image">
+            <div class="desc">BRAZIL</div>
+    </div>
+    
+    <div class="gallery">
+        <img src="img/CRica.jpg" width="1000" height="600" alt="image">
+        <div class="desc">COSTA RICA</div>
+    </div>      
+    
+    <div class="gallery">
+        <img src="img/Hawaii1.jpg" width="1000" height="600" alt="image">
+        <div class="desc">HAWAII</div>
+    </div>             
+
+    <div class="gallery">
+        <img src="img/Phil1.jpg" width="1000" height="600" alt="image">
+        <div class="desc">PHILIPPINES</div>
+    </div>        
+
+    <div class="gallery">
+        <img src="img/Brazil2.jpg" width="1000" height="600" alt="image">
+        <div class="desc">BRAZIL</div>
+    </div>        
+
+    <div class="gallery">
+        <img src="img/CRica2.jpg" width="1000" height="600" alt="image">
+        <div class="desc">COSTA RICA</div>
+    </div>      
+
+    <div class="gallery">
+        <img src="img/Hawaii2.jpg" width="1000" height="600" alt="image">
+        <div class="desc">HAWAII</div>
+    </div>         
+
+    <div class="gallery">
+        <img src="img/Phil2.jpg" width="1000" height="600" alt="image">
+        <div class="desc">PHILIPPINES</div>
+    </div>     
+
+    <div class="gallery">
+        <img src="img/Brazil3.jpeg" width="1000" height="600" alt="image">
+        <div class="desc">BRAZIL</div>
+    </div>         
+
+    <div class="gallery">
+        <img src="img/CRica3.jpg" width="1000" height="600" alt="image">
+        <div class="desc">COSTA RICA</div>
+    </div>                  
+
+    <div class="gallery">
+        <img src="img/Hawaii3.jpg" width="1000" height="600" alt="image">
+        <div class="desc">HAWAII</div>
+    </div> 
+    
+    <div class="gallery">
+        <img src="img/Phil3.jpg" width="1000" height="600" alt="image">
+        <div class="desc">PHILIPPINES</div>
+    </div>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <div></div>
+
+    <div>
+        <h1>Captain Pineapple's Homepage</h1>
+        <hr/>
+    </div>
+
+    <div class="mainpx">
+        <img src="img/pineapple3.jpeg" height="300px" width="400px"  border="1px" alt=" " /><br/>   
+    </div>
+    
+    <div class="intro" link rel="preconnect" href="https://fonts.googleapis.com">
+    <p>
+        Captain Pineapple wishes to Welcome you to the home of the best fruit in the world!
+        
+    </p>
+    </div>
+    <hr/>
+
+    <br/>
+
+    <div>
+        <button type="button"><a href= about-me.php target="_blank"> About Me </a></button>
+        <button type="button"><a href= gallery.php target="_blank"> Gallery </a></button>
+        <button type="button"><a href= contact.php target="_blank">Contact </a></button>
+    </div>
+
+    <div class="ppp2">
+        <!-- <img src="pineapple21.jpg" height="50%" width="100%" alt=" "> -->
+        <img src="img/pineapple11.jpg" height="50%" width="100%" alt=" ">
+    </div>
+
+    <div class="intro2" link rel="preconnect" href="https://fonts.googleapis.com">
+        <p>
+            Sit back, Relax, and Drink some Juice!
+        </p>
+
+    </div>
+
+    <br/>
+
+    <svg width="1000" height="250">
+        <rect with="150" height="150" fill="orange">
+            <animate attributeName ="x" from ="0" to ="300" dur ="3s" fill ="freeze" repeatCount ="indefinite"/>
+        </rect>
+    </svg>
+
+    <footer>
+        Normally no one EVER reads this part of the webpage which contains boring stuff like Privacy Policy, Terms and Conditions, and Copy Right Information. #SayNoToFooters 
+    </footer>
+    
+</body>
+</html>
